cb59542764b3558bcdf4ce7e7939edfe9222ec7f
Yashh-11 <yashchandrani11@gmail.com>
2026-01-27 20:13:30 -0500

    updated3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import jsonServer from 'json-server';
 import cors from 'cors';
+import fs from 'fs';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -7,6 +8,16 @@ const middlewares = jsonServer.defaults();
 
 server.use(cors());
 server.use(middlewares);
+
+// Middleware to auto-save changes to db.json
+router.render = (req, res) => {
+  // After each request, save the database
+  if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method)) {
+    const db = router.db.getState();
+    fs.writeFileSync('db.json', JSON.stringify(db, null, 2));
+  }
+  res.jsonp(res.locals.data);
+};
 
 // Use default router
 server.use(router);
